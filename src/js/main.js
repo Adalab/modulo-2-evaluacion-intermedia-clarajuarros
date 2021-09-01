c96e71bffa8f2581bcd09fc7799018a60f6dcb14
clarajuarros <clarajuarross@gmail.com>
2021-09-01 10:32:37 +0200

    add javaScript

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,39 @@
 'use strict';
 
-console.log('>> Ready :)');
+ const Number = document.querySelector(".numero");
+const btnPrueba = document.querySelector(".btnPrueba");
+const pista = document.querySelector(".pista");
+const numeroRespuestas = document.querySelector(".numeroRespuestas");
+
+let guessCount = 1;
+let resetButton;
+
+function getRandomNumber(max) {
+    return Math.ceil(Math.random() * max);
+}
+
+function verificar(){
+    var numero =Number.value;
+}
+
+function checkGuess (){
+    let userGuess = Number(Number.value);
+if (numeroRespuestas === 1){
+    guesses.numeroRespuestas = "Intentos anteriores: ";
+}
+guesses.numeroRespuestas += numeroRespuestas + " ";
+
+if (Number === getRandomNumber){
+    pista = "Has ganado campeona!!"
+    setGameOver()
+}
+if (Number < getRandomNumber){
+    pista = "demasiado bajo";
+}
+if (Number > getRandomNumber){
+    pista = "demasiado alto";
+}
+if (Number > 100){
+    pista = "El número debe estar entre 1 y 100"
+}
+}
